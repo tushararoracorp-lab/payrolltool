ffe70e2ad0051e07914270fdebdf95988724a3a8
TusharArora <tushar.arora.corp@gmail.com>
2026-06-21 17:18:03 +0530

    fix: add GA4 tracking to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,37 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+
+const GA_ID = G-YZKNE00CWX;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", GA_ID, { page_path: url });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
